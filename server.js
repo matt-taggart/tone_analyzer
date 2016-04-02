0910dd73adc98b9afaf0e6c0734312b7b674efeb
unknown <jeffreyhshapiro@gmail.com>
2016-04-02 12:48:59 -0400

    beginning to create data schema for saving tone analysis

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var logger = require('morgan');
 var watson = require('watson-developer-cloud');
+var mongoose = require('mongoose');
+// var tonez = require('./tone_analyzer_intro.js')
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -8,7 +10,10 @@ var PORT = process.env.PORT || 3000;
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
- 
+var db = 'mongodb://localhost/ToneDownForWhatDB'
+var ContentDB = require('./model/contentAnalysisModel.js')
+mongoose.connect(db)
+
 // var alchemy_language = watson.alchemy_language({
 //   api_key: '<api_key>'
 // });
@@ -23,6 +28,64 @@ app.use(express.static(__dirname + '/public'));
 //   else
 //     console.log(JSON.stringify(response, null, 2));
 // });
+
+var tone_analyzer = watson.tone_analyzer({
+  url: "https://gateway.watsonplatform.net/tone-analyzer-beta/api",
+  password: "B0jj6n5i9NXZ",
+  username: "a52b4d31-5e94-4acb-bbbe-7de263677456",
+  version: 'v3-beta',
+  version_date: '2016-02-11'
+});
+
+var toneText = 'One fish, two fish, red fish, blue fish'
+
+tone_analyzer.tone({ text: toneText },
+  function(err, tone) {
+    if (err)
+      console.log(err);
+    else
+      // console.log(JSON.stringify(tone.document_tone.tone_categories[0].tones, null, 2));
+      // console.log(tone.document_tone.tone_categories[1].tones)
+
+    var emotionToneArray = []
+    var writingToneArray = []
+    var socialToneArray = []
+
+  for (var i = 0; i < tone.document_tone.tone_categories[0].tones.length; i++) {
+    emotionToneArray.push({
+      tone_type: tone.document_tone.tone_categories[0].tones[i].tone_name, 
+      tone_score: tone.document_tone.tone_categories[0].tones[i].score 
+    })
+  };
+
+  for (var i = 0; i < tone.document_tone.tone_categories[1].tones.length; i++) {
+    writingToneArray.push({
+      tone_type: tone.document_tone.tone_categories[1].tones[i].tone_name, 
+      tone_score: tone.document_tone.tone_categories[1].tones[i].score 
+    })
+  };
+
+  for (var i = 0; i < tone.document_tone.tone_categories[2].tones.length; i++) {
+    socialToneArray.push({
+      tone_type: tone.document_tone.tone_categories[2].tones[i].tone_name, 
+      tone_score: tone.document_tone.tone_categories[2].tones[i].score 
+    })
+  };
+      // console.log(tone.document_tone.tone_categories[0].tones[i].score)
+      // console.log(tone.document_tone.tone_categories[0].tones[i].tone_name)
+    
+  //Save to DB- put into a post request
+  var content = new ContentDB ({
+    content: toneText,
+    emotion_tone_data: emotionToneArray,
+    social_tone_data: socialToneArray,
+    writing_tone_data: writingToneArray
+  })
+  content.save(function(err, response){
+    if (err) {throw err}
+    return response
+  })
+});
 
 app.use('*', function(req, res) {
   res.send('hello world!');
--- tone_analyzer_intro.js
+++ tone_analyzer_intro.js
@@ -1,4 +1,9 @@
 var watson = require('watson-developer-cloud');
+var mongoose = require('mongoose');
+var db = 'mongodb://localhost/ToneDownForWhatDB'
+
+var ContentDB = require('./model/contentAnalysisModel.js')
+mongoose.connect(db)
 
 var tone_analyzer = watson.tone_analyzer({
   url: "https://gateway.watsonplatform.net/tone-analyzer-beta/api",
@@ -14,4 +19,15 @@ tone_analyzer.tone({ text: 'A word is dead when it is said, some say. Emily Dick
       console.log(err);
     else
       console.log(JSON.stringify(tone, null, 2));
+      // for (var i = 0; i < tone.document_tone.tone_categories.length; i++) {
+
+      //   for (var j = 0; j < tone.document_tone.tone_categories.tones.length; j++) {
+      //     console.log(tone.document_tone.tone_categories.tones[j])
+      //   };
+
+        // var content = new ContentDB({
+        //   content : text,
+        //   tone_score: [tone_type: ]
+        // })
+      // };
 });
