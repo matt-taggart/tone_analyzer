d04f09f1772dc584e05f2b0a2945aab5a1192c62
unknown <jeffreyhshapiro@gmail.com>
2016-04-03 10:59:55 -0400

    data rendering to webpage

--- public/app.js
+++ public/app.js
@@ -5,27 +5,13 @@ angular.module("toneDown", [])
       $http.post('/tonetext', {
         content: $scope.toneText
       }).then(function(){
-        $scope.textTone = '';
+        $scope.toneText = '';
+        $scope.retrieveData();
       })
     }
     $scope.retrieveData = function(){
       $http.get('/calldata').then(function(response){
-
         $scope.toneDatas = response.data
-        // console.log(response.data)
-
-        angular.forEach(response.data, function(value, key){
-          $scope.toneDatas.emotionData = value.emotion_tone_data
-          console.log($scope.toneDatas.emotionData)
-          $scope.toneDatas.writingData = value.writing_tone_data
-          console.log($scope.toneDatas.writingData)
-          $scope.toneDatas.socialData = value.social_tone_data
-          console.log($scope.toneDatas.socialData)
-        })
-        debugger;
-        // console.log(response.data)
-        
-
       })
     }
   })
