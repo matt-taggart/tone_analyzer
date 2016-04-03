f4efba81a72f2c94b2cc3bbbc6f84a485af09680
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-04-03 16:05:12 -0400

    Created local strategy for login sessions.

--- config/passport.js
+++ config/passport.js
@@ -4,20 +4,38 @@ var User = require('../models/users.js');
 
 module.exports = function(passport) {
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(user, function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
   });
-});
 
-passport.use('login', LocalStrategy {
-  passReqToCallback: true
-}, function(req, username, password, done) {
-  
-});
+  passport.deserializeUser(user, function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use('login', LocalStrategy {
+    passReqToCallback: true
+  }, function(req, username, password, done) {
+    User.findOne({ username: username}).exec(function(err, userData) {
+      if (err) {
+        return err
+      }
+
+      if (!userData) {
+        done(null, false);
+      }
+
+      if (userData) {
+        bcrypt.compare(password, userData.password, function(err, user) {
+          if (user) {
+            done(null, user);
+          } else {
+            return err;
+          }
+        });
+      }
+    });
+  });
 
 }
