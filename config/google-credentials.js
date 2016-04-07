1519e84c817cd52919131786b3ea1aebf52c1c83
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-04-07 16:27:09 -0400

    Reset app secret and added credentials to .gitignore file.

--- config/google-credentials.js
+++ config/google-credentials.js
@@ -1,8 +1,7 @@
 var googleCredentials = {
   clientId:  '138716791581-csqro8n6ov7haaic420u3r7t41lja48q.apps.googleusercontent.com',
-  clientSecret: 'H0XCSwJoxxaYUBTw-e0jecNn',
+  clientSecret: 'FS8m0OxlXO3RC_XESSACAUW1',
   callbackURL: 'http://toneanalyzer.dev:3000/auth/google/callback'
 }
-
 
 module.exports = googleCredentials;
--- controllers/googleOAuth.js
+++ controllers/googleOAuth.js
@@ -2,4 +2,4 @@ var google = require('googleapis');
 var googleCredentials = require('google-credentials.js')
 var OAuth2 = google.auth.OAuth2;
 
-var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var oauth2Client = new OAuth2(googleCredentials.clientId, googleCredentials.clientSecret, googleCredentials.callbackURL);
