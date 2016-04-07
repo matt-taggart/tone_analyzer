5252804ed8a537ed53486bdb88105c4949418b78
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-04-07 16:33:41 -0400

    Successfully generated URL for oAuth2 using google credentials.

--- controllers/googleOAuth.js
+++ controllers/googleOAuth.js
@@ -1,5 +1,16 @@
 var google = require('googleapis');
-var googleCredentials = require('google-credentials.js')
+var googleCredentials = require('../config/google-credentials.js')
 var OAuth2 = google.auth.OAuth2;
 
 var oauth2Client = new OAuth2(googleCredentials.clientId, googleCredentials.clientSecret, googleCredentials.callbackURL);
+
+var scopes = [
+  'https://www.googleapis.com/auth/gmail.readonly'
+];
+
+var url = oauth2Client.generateAuthUrl({
+  access_type: 'online',
+  scope: scopes
+});
+
+console.log(url);
