2b793d60cca764e5ba4c5e64abb87537faff37b4
unknown <jeffreyhshapiro@gmail.com>
2016-04-06 21:01:14 -0400

    highcharts not rendering to webpage

--- public/inputFormController.js
+++ public/inputFormController.js
@@ -12,48 +12,70 @@ angular.module("toneDown")
       $http.get('/calldata').then(function(response){
         $scope.toneDatas = response.data
 
-        var highChartToneData = [];
-
         var idArray = []
 
+        angular.element(document).ready(function(){
+
         angular.forEach($scope.toneDatas, function(value, key) {
-
-          idArray.push(value._id)
-
+          idArray.push({id: value._id, social_tone_data: value.social_tone_data})
           for (var i = 0; i < idArray.length; i++) {
-            console.log(idArray[i])
-            console.log(idArray.length)
-          };
-
-            // angular.forEach(value.social_tone_data, function(value, key) {
-
-              // highChartToneType.push(value.tone_type)
-              // highChartToneScore.push(value.tone_score) 
-              // $("#" + value._id).highcharts({
-              //   chart: {
-              //       type: 'bar'
-              //   },
-              //   title: {
-              //       text: 'Fruit Consumption'
-              //   },
-              //   xAxis: {
-              //       categories: highChartToneType
-              //   },
-              //   yAxis: {
-              //       title: {
-              //           text: 'Fruit eaten'
-              //       }
-              //   },
-              //   series: [{
-              //       data: highChartToneScore
-              //   }]
-              // });
-            // })
+            if (idArray[i].id === value._id) {
+              console.log(idArray[i].id)
+              var socialToneName = []
+              var socialToneScore = []
+              angular.forEach(value.social_tone_data, function(value, key) {
+                socialToneScore.push(value.tone_score)
+                socialToneName.push(value.tone_type)
+              })
 
 
-        })
-// console.log(idArray)
-        
+              var chartInfo = {
+                chart: {
+                    type: 'bar',
+                    renderTo: 'container'
+                },
+                title: {
+                    text: 'Fruit Consumption'
+                },
+                xAxis: {
+                    categories: socialToneName
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    data: socialToneScore
+                }]
+              }
+            }
+
+            var chart = new Highcharts.chart(chartInfo)
+
+          }
+
+          // $("#container").highcharts({
+          //   chart: {
+          //       type: 'bar'
+          //   },
+          //   title: {
+          //       text: 'Fruit Consumption'
+          //   },
+          //   xAxis: {
+          //       categories: socialToneName
+          //   },
+          //   yAxis: {
+          //       title: {
+          //           text: 'Fruit eaten'
+          //       }
+          //   },
+          //   series: [{
+          //       data: socialToneScore
+          //   }]
+          // });
+        });
+      })
       });
-    };
+    }
   });
