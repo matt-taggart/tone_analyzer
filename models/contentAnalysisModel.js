a996811a3922577a05a2372244769d79402466d6
unknown <jeffreyhshapiro@gmail.com>
2016-04-17 18:01:32 -0400

    added user reference in contentAnalysisModel.js

--- models/contentAnalysisModel.js
+++ models/contentAnalysisModel.js
@@ -2,11 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema
 
 var contentSchema = new Schema({
+  name: String,
   content: String,
   emotion_tone_data: [{tone_type: String, tone_score: Number}],
   writing_tone_data: [{tone_type: String, tone_score: Number}],
-  social_tone_data: [{tone_type: String, tone_score: Number}]
-  // user: [{type: Schema.Types.ObjectId, ref: 'UsersDB'}]
+  social_tone_data: [{tone_type: String, tone_score: Number}],
+  userId: {
+    type: Schema.Types.ObjectId, 
+    ref: 'User'
+  }
 });
 
 module.exports = mongoose.model('Content', contentSchema)
